2f57b4318607b3aee0aca8ec9669ca481d5a35fe
yamasaki <shohei615615@gmail.com>
2024-02-04 00:23:00 +0800

    e-tiga add q and a section

--- daytora/e-taiga/js/script.js
+++ daytora/e-taiga/js/script.js
@@ -4,3 +4,21 @@ $("#js-drawer-icon").on("click", function (e) {
   $("#js-drawer-icon").toggleClass("is-checked");
   $("#js-drawer-content").toggleClass("is-checked");
 });
+
+/*
+summaryをクリックした時にopen属性があったらslideUp()でコンテンツ隠す
+open属性がなかったらslideDown()でコンテンツ表示
+open属性がついたらマイナスボタンにする
+*/
+
+$(".js-accordion").on("click", function (e) {
+  e.preventDefault();
+
+  if ($(this).parent().hasClass("is-open")) {
+    $(this).parent().removeClass("is-open");
+    $(this).next().slideUp();
+  } else {
+    $(this).parent().addClass("is-open");
+    $(this).next().slideDown();
+  }
+});
